f78e598acc9a4cde0ac700a73b388767064544c0
Vincent Tonkes <vincenttonkes@gmail.com>
2022-05-14 12:41:10 +0200

    Made the training and testing pretty much ready for Peregrine :-D

--- rijks_torch/training/test.py
+++ rijks_torch/training/test.py
@@ -3,31 +3,25 @@ from torch import nn
 import pandas as pd
 from ..data_loading import RijksDataloaders
 
-def test(model: nn.Module, dataloaders: RijksDataloaders, lossfunc):
+def test(model: nn.Module, dataloaders: RijksDataloaders, lossfunc, name: str = "default"):
     """
     ### Tests the given model on dataloaders.test.
     Saves the following data as csv-files:\n
-    \t(1) 'test_predictions.csv' gives the full softmax prediction, as well as the correct output;\n
-    \t(2) 'test_confusion.csv' gives the confusion matrix.\n
+    \t(1) '{name}-test_predictions.csv' gives the full softmax prediction, as well as the correct output;\n
+    \t(2) '{name}-test_confusion.csv' gives the confusion matrix.\n
     """
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     model.to(device)
     model.eval()
 
-    print("TESTING HAS STARTED!")
-
     # Gives the softmax predictions of the model, plus the actual output:
     dfPredictions = pd.DataFrame(columns=dataloaders.materials + ["actual_idx", "actual", "batch_loss"])
 
     # Gives the confusion matrix, with rows showing predictions, and colums what it was supposed to be
+    # I realize that this is the wrong way around.. Transposing it for analysis en ploting later on.
     confdims = len(dataloaders.materials)
     confusion = torch.zeros((confdims, confdims), dtype=torch.int)
-
-    # Statistics to output at the end
-    running_accuracy = 0.0
-    running_loss = 0.0
-    count = 0
 
     with torch.no_grad():
         for x, y in dataloaders.test:
@@ -38,7 +32,7 @@ def test(model: nn.Module, dataloaders: RijksDataloaders, lossfunc):
             pred = model(x).softmax(dim=1)
             loss = lossfunc(pred, y)
 
-            # Update statistics (more precise than for training: taking size of last batch into account):
+            # Update statistics:
             for p, a in zip(pred, y):
                 newRow = pd.DataFrame(
                     [list(p.cpu().numpy()) + [a.item(), dataloaders.materials[a.item()], loss.item()]],
@@ -47,24 +41,13 @@ def test(model: nn.Module, dataloaders: RijksDataloaders, lossfunc):
                 dfPredictions = pd.concat([dfPredictions, newRow], ignore_index=True)
 
                 confusion[p.argmax().item()][a.item()] += 1
-
-                # Updating statistics to show at the end:
-                count += 1
-                running_accuracy += (1 / count) * ((p.argmax() == a).type(torch.float).item() - running_accuracy)
-                running_loss += (1 / count) * (loss.item() - running_loss)
-
-            print(f"\033[F\033[KAccuracy: {running_accuracy:0.3f}; Mean loss: {running_loss:0.3f}")
-    
+  
     # Saving predictions
-    dfPredictions.to_csv("test_predictions.csv", index=False)
+    dfPredictions.to_csv(f"{name}-test_predictions.csv", index=False)
 
     # Creating dataframe from the confusion matrix and saving it:
     dfConf = pd.DataFrame(confusion.numpy(), columns=dataloaders.materials)
     dfConf["predictions"] = dataloaders.materials
     columns = dfConf.columns.to_list()
     dfConf = dfConf[columns[-1:] + columns[:-1]]
-    dfConf.to_csv("test_confusion.csv", index=False)
-
-    print("TESTING HAS ENDED!\n"
-          "   Results saved to 'test_predictions.csv' and 'test_confusion.csv'\n"
-         f"   Final accuracy was: {running_accuracy:0.3f}; Mean loss was: {running_loss:0.3f}")
+    dfConf.to_csv(f"{name}-test_confusion.csv", index=False)
--- rijks_torch/training/train.py
+++ rijks_torch/training/train.py
@@ -6,7 +6,7 @@ from ..data_loading import RijksDataloaders
 from time import time
 from copy import deepcopy
 
-def train(model: nn.Module, dataloaders: RijksDataloaders, lossfunc, optimizer, max_epochs: int, early_stop: int = 10):
+def train(model: nn.Module, dataloaders: RijksDataloaders, lossfunc, optimizer, max_epochs: int = 200, early_stop: int = 7, name: str = "default"):
     """
     ## Function for training of a model
 
@@ -15,76 +15,56 @@ def train(model: nn.Module, dataloaders: RijksDataloaders, lossfunc, optimizer, 
     lossfunc is the used loss function.\n
     optimizer is the used optimizer.\n
     max_epochs maximum number of epochs if no early stopping occured\n
-    early_stop if no new maximum is found after this many trials, it stops\n
+    early_stop if no new best is found after this many trials, training stops\n
+    name is the name of the experiment. Gets prepended to output filenames\n
     ### Returns the model that scored best on the validation set!\n
-    ### Also saves statistics of first epoch to first_epoch.csv and validation statistics to validation.csv
+    ### Also saves validation statistics to {name}-validation.csv
     """
     
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device)
     
-    print(f"___TRAINING_STARTED_({device.upper()})___" + "_" * 40)
     startTime = time()
 
-    # Keeping track of the best model
+    # Keeping track of the best model:
     best_model = deepcopy(model.state_dict())
-    best_accuracy = 0.0
+    best_loss = float('inf')
     best_epoch = 0
 
-    # Record statistics of first batch to this file:
-    first_epoch_log = "first_epoch.csv"
+    # File to record validation statistics to:
+    validation_log = f"{name}-validation.csv"
 
     # Preparing the validation csv file:
-    with open("validation.csv", "a") as f:
+    with open(validation_log, "a") as f:
         f.write("accuracy,mean_loss\n")
 
     for epoch in range(max_epochs):
-        print(f"---EPOCH-{(epoch + 1):03}-OF-{max_epochs:03}-" + "-" * 30)
+        train_loop(model, dataloaders.train, lossfunc, optimizer, device)
+        loss = validation_loop(model, dataloaders.val, lossfunc, device, validation_log)
 
-        train_loop(model, dataloaders.train, lossfunc, optimizer, device, first_epoch_log)
-        accuracy = validation_loop(model, dataloaders.val, lossfunc, device)
-
-        # Set file to None so subsequent epochs won't be recorded:
-        # Reason behind this is that these epochs have seen all instances before
-        # so the statistics are less meaningfull.
-        first_epoch_log = None
-
-        if accuracy > best_accuracy:
-            print("New best model! Saving parameters.")
+        # Save best model and early stop if no new best for too long:
+        if loss < best_loss:
             best_model = deepcopy(model.state_dict())
-            best_accuracy = accuracy
+            best_loss = loss
             best_epoch = epoch
-            torch.save(best_model, "best.pth")
+            torch.save(best_model, f"{name}-best.pth")
         elif epoch - best_epoch >= early_stop:
-            print("Early stop occured.")
             break
 
     endTime = time()
-    print("___TRAINING_ENDED_____" + "_" * 40)
-    print(f"Total training time was: {(endTime - startTime):0.3f} seconds.")
-    print(f"The best model had an accuracy of: {best_accuracy:0.3f}.")
+    with open(f"{name}-trainingtime.txt", "w") as f:
+        f.write(f"Training time: {endTime - startTime}")
 
     # Return the best model found:
     model.load_state_dict(best_model)
     return model
 
-def train_loop(model: nn.Module, dataloader: DataLoader, lossfunc, optimizer, device, savefile):
+def train_loop(model: nn.Module, dataloader: DataLoader, lossfunc, optimizer, device):
     model.train()
-    percentDone = -1
-    num_batches = len(dataloader)
-
-    running_accuracy = 0.0
-    running_loss = 0.0
-
-    # Adding column names to csv file to save to:
-    if savefile != None:
-        with open(savefile, "a") as f:
-            f.write("batch_size,accuracy,mean_loss\n")
 
     for batchnum, (x, y) in enumerate(dataloader):
         x = x.to(device)
         y = y.to(device)
-        batch_size = len(y)
 
         # Forward pass:
         pred = model(x)
@@ -95,49 +75,7 @@ def train_loop(model: nn.Module, dataloader: DataLoader, lossfunc, optimizer, de
         loss.backward()
         optimizer.step()
 
-        # Update statistics (neglecting fact that last batch is smaller):
-        batch_acc = (pred.argmax(1) == y).type(torch.float).sum().item() / batch_size
-        running_accuracy += (1 / (batchnum + 1)) * (batch_acc - running_accuracy)
-        running_loss += (1 / (batchnum + 1)) * (loss.item() - running_loss)
-
-        # Saving statistics to file if filename given:
-        if savefile != None:
-            with open(savefile, "a") as f:
-                f.write(f"{batch_size},{batch_acc},{loss.item()}\n")
-
-        # Update display 100 times per epoch:
-        tmpPercent = int(100 * batchnum / num_batches)
-        if tmpPercent > percentDone:
-            percentDone = tmpPercent
-            showProgress(percentDone, running_loss, running_accuracy)
-
-def showProgress(percentDone: int, running_loss: float, running_acc: float):
-    """Just showing off :-p Having a nice function to show progress"""
-    total_bars = 35
-    num_bars = int(total_bars * percentDone / 100)
-    
-    # Moving up two lines if it isn't the first time:
-    if (percentDone != 0):
-        print("\033[2F", end="")
-    
-    # Clear line:
-    print("\033[2K", end="")
-
-    # Printing a loading bar:
-    print("(", end="")
-    for i in range(num_bars):
-        print("=", end="")
-    for i in range(total_bars - num_bars):
-        print(" ", end="")
-    print(f") {percentDone}% done!")
-
-    # Clear line:
-    print("\033[2K", end="")
-
-    # Printing loss and accuracy, and going back to beginning of previous line:
-    print(f"Training loss: {running_loss:0.3f}; Training accuracy: {running_acc:0.3f}.", flush=True)
-
-def validation_loop(model: nn.Module, dataloader: DataLoader, lossfunc, device):
+def validation_loop(model: nn.Module, dataloader: DataLoader, lossfunc, device, validation_csv):
     model.eval()
 
     running_accuracy = 0.0
@@ -153,22 +91,14 @@ def validation_loop(model: nn.Module, dataloader: DataLoader, lossfunc, device):
             pred = model(x)
             loss = lossfunc(pred, y)
 
-            # Update statistics (more precise than for training: taking size of last batch into account):
+            # Update statistics:
             for p, a in zip(pred, y):
                 count += 1
                 running_accuracy += (1 / count) * ((p.argmax() == a).type(torch.float).item() - running_accuracy)
                 running_loss += (1 / count) * (loss.item() - running_loss)
 
-    # Print statistics:
-    print(
-f"""Validation statistics:
-    Accuracy: {running_accuracy:0.3f}
-    Loss:     {running_loss:0.3f}
-"""
-    )
-
     # Save statistics to validation file:
-    with open("validation.csv", "a") as f:
+    with open(validation_csv, "a") as f:
         f.write(f"{running_accuracy},{running_loss}\n")
 
-    return running_accuracy
+    return running_loss
