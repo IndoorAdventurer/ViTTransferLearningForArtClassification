9fc23bda0b94e84931e681380d5b0c19746d78e4
Vincent Tonkes <vincenttonkes@gmail.com>
2022-05-20 14:00:55 +0200

    Did the second experiment and analyzed the results

--- ots_experiment.sh
+++ ots_experiment.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
 #SBATCH --array=1-8
@@ -15,10 +15,10 @@ module purge
 module load Python/3.9.6-GCCcore-11.2.0
 
 mkdir -p /local/tmp/dataset
-tar xzf /data/$USER/jpg_fullsize.tar.gz -C /local/tmp/dataset
+tar xzf /data/$USER/datasets/$2.tar.gz -C /local/tmp/dataset
 
 infile=ots_models.txt
 model=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $infile)
 
 source /data/$USER/bachproj/bin/activate
-python ots_experiment.py ots_mat1_$model $model /local/tmp/dataset/fullsize /local/tmp/dataset
+python ots_experiment.py $1_$model $model /local/tmp/dataset/fullsize /local/tmp/dataset
